f62e60a3cb650cbcd6a7b6d1519ac79453f4c8e9
Rucha Khadatkar <ruchakhadatkar83@gmail.com>
2024-03-14 12:30:32 +0530

    bugs fixed

--- script.js
+++ script.js
@@ -1,17 +1,39 @@
 const userInput = document.querySelector("input");
 const form = document.querySelector("form");
+const result = document.querySelector(".result");
+const allGuess = document.querySelector(".allGuesses");
+const submitBtn = document.querySelector(".submitBtn");
+const startGameBtn = document.querySelector(".startGame");
 
-let randomNumber = Math.round(Math.random() * 100);
+function a() {
+  const allGuessArray = [];
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const userInputValue = parseInt(userInput.value);
-  if (userInputValue < randomNumber) {
-    console.log("Too Low");
-  } else if (userInputValue > randomNumber) {
-    console.log("Too High");
-  } else {
-    console.log("Congrats");
-  }
-  userInput.value = "";
-});
+  let randomNumber = Math.round(Math.random() * 100);
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const userInputValue = parseInt(userInput.value);
+    if (userInputValue < randomNumber) {
+      result.innerText = "Too Low !";
+    } else if (userInputValue > randomNumber) {
+      result.innerText = "Too High !";
+    } else {
+      result.innerText = "You got it, Congrats !!!";
+      startGameBtn.disabled = false;
+      submitBtn.disabled = true;
+    }
+    allGuessArray.push(userInputValue);
+    allGuess.innerText = "Your Guesses : " + allGuessArray.join(" ,");
+    form.reset();
+  });
+
+  startGameBtn.addEventListener("click", () => {
+    allGuess.innerText = "";
+    result.innerText = "";
+    startGameBtn.disabled = true;
+    submitBtn.disabled = false;
+    randomNumber = Math.round(Math.random() * 100);
+  });
+}
+
+a();
